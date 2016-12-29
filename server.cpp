fa287623b232b203e3e4a003355815ffd1de4f1d
Sean Bollin <sean@sean-bollin.com>
2016-12-29 15:50:18 -0800

    test broadcast server

--- server.cpp
+++ server.cpp
@@ -1,22 +1,54 @@
-#include <iostream>
+#include <set>
 
 #include <websocketpp/config/asio_no_tls.hpp>
 #include <websocketpp/server.hpp>
 
 using server = websocketpp::server<websocketpp::config::asio>;
 
-void on_message(websocketpp::connection_hdl hdl, server::message_ptr msg) {
-	std::cout << "received message: " << msg->get_payload() << std::endl;
-}
+using websocketpp::connection_hdl;
+using websocketpp::lib::placeholders::_1;
+using websocketpp::lib::placeholders::_2;
+using websocketpp::lib::bind;
+
+class broadcast_server {
+public:
+    broadcast_server() {
+        m_server.init_asio();
+                
+        m_server.set_open_handler(bind(&broadcast_server::on_open,this,::_1));
+        m_server.set_close_handler(bind(&broadcast_server::on_close,this,::_1));
+        m_server.set_message_handler(bind(&broadcast_server::on_message,this,::_1,::_2));
+    }
+    
+    void on_open(connection_hdl hdl) {
+        m_connections.insert(hdl);
+    }
+    
+    void on_close(connection_hdl hdl) {
+        m_connections.erase(hdl);
+    }
+    
+    void on_message(connection_hdl hdl, server::message_ptr msg) {
+   		std::cout << "received message: " << msg->get_payload() << std::endl;
+        for (auto it : m_connections) {
+        	std::cout << "sending message ..." << std::endl;
+            m_server.send(it,msg);
+        }
+    }
+
+    void run(uint16_t port) {
+        m_server.listen(port);
+        m_server.start_accept();
+        m_server.run();
+    }
+private:
+    typedef std::set<connection_hdl,std::owner_less<connection_hdl>> con_list;
+
+    server m_server;
+    con_list m_connections;
+};
 
 int main() {
-    server print_server;
-
-    print_server.set_message_handler(&on_message);
-
-    print_server.init_asio();
-    print_server.listen(9002);
-    print_server.start_accept();
-
-    print_server.run();
+    broadcast_server server;
+    server.run(9002);
 }
