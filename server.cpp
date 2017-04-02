1cd13f5153fe2dbcd0832ac3df698e3c4532c717
Sean Bollin <sean@sean-bollin.com>
2017-04-01 19:30:55 -0700

    remove unused files

--- server.cpp
+++ server.cpp
@@ -1,24 +1,31 @@
 #include <beast/websocket.hpp>
-#include <boost/bind.hpp>
 #include <boost/enable_shared_from_this.hpp>
 
 #include <iostream>
 
-using boost::asio::ip::tcp;
-
-class ws_connection : public boost::enable_shared_from_this<ws_connection>
+class WSConnection : public std::enable_shared_from_this<WSConnection>
 {
 public:
-    typedef boost::shared_ptr<ws_connection> pointer;
+    using boost::asio::ip::tcp;
 
-    static pointer create(boost::asio::io_service& io_service)
+    // TODO: Make ctor private
+    WSConnection(boost::asio::io_service& io_service) : ws_(io_service) {}
+
+    static auto create(boost::asio::io_service& io_service)
     {
-        return pointer(new ws_connection(io_service));
+        return std::make_shared<WSConnection>(io_service);
     }
 
     void handshake()
     {
-        ws_.async_accept(boost::bind(&ws_connection::handle_accept, shared_from_this()));
+        auto self = shared_from_this();
+        ws_.async_accept([self]() {
+                    std::cout << "handle_accept" << std::endl;
+                    beast::streambuf buf;
+                    buf << "Hello from obsidian-server!"; // TODO: This data is going to die?
+                    ws_.async_write(buf.data(), [self]() {});
+                }
+        );
     }
 
     tcp::socket& socket()
@@ -27,29 +34,14 @@ public:
     }
 
 private:
-    ws_connection(boost::asio::io_service& io_service) : ws_(io_service)
-    {
-        response_ << "Hello from obsidian-server!";
-    }
-
-    void handle_accept() {
-        std::cout << "handle_accept" << std::endl;
-        ws_.async_write(response_.data(), boost::bind(&ws_connection::handle_write, shared_from_this()));
-    }
-
-    void handle_write() {
-        std::cout << "handle_write" << std::endl;
-    }
-
     beast::websocket::stream<boost::asio::ip::tcp::socket> ws_;
-    beast::streambuf response_;
 };
 
-class rubymud_server {
+class RubyMUDServer {
 public:
-    using error_code = boost::system::error_code;
+    using boost::asio::ip::tcp;
 
-    rubymud_server(boost::asio::io_service& io_service) : acceptor_(io_service, tcp::endpoint(tcp::v4(), 33435))
+    RubyMUDServer(boost::asio::io_service& io_service) : acceptor_(io_service, tcp::endpoint(tcp::v4(), 33435))
     {
         start_accept();
     }
@@ -57,27 +49,21 @@ public:
 private:
     void start_accept()
     {
-        ws_connection::pointer new_connection = ws_connection::create(acceptor_.get_io_service());
-        acceptor_.async_accept(new_connection->socket(),
-                boost::bind(
-                        &rubymud_server::handle_accept,
-                        this,
-                        new_connection,
-                        boost::asio::placeholders::error
-                )
+        auto connection = WSConnection::create(acceptor_.get_io_service());
+        acceptor_.async_accept(
+                connection->socket(),
+                [connection,this] (auto error) {
+                    std::cout << "handle_accept" << std::endl;
+                    if (!error) {
+                       connection->handshake();
+                    } else {
+                       std::cout << "error: error_code value " << error.value() << std::endl;
+                    }
+
+                    this->start_accept();
+                }
         );
-    }
 
-    void handle_accept(ws_connection::pointer new_connection, const error_code& error) {
-        std::cout << "handle_accept" << std::endl;
-
-        if (!error) {
-            new_connection->handshake();
-        } else {
-            std::cout << "error: error_code value " << error.value() << std::endl;
-        }
-
-        start_accept();
     }
 
     tcp::acceptor acceptor_;
@@ -100,7 +86,7 @@ int main(int argc, char* argv[]) {
 
     try {
         boost::asio::io_service io_service;
-        rubymud_server server(io_service);
+        RubyMUDServer server(io_service);
         io_service.run();
         // sig_wait();
     } catch (std::exception& e) {
